90fbfc5ebddfbd6e04c3e80abd52447b6b14a860
MatheusRR01 <vector.320@outlook.com>
2024-08-09 22:27:21 -0300

    Add image

--- appflatlist/App.js
+++ appflatlist/App.js
@@ -5,6 +5,7 @@ import {
   FlatList,
   StyleSheet,
   Text,
+  Image,
   StatusBar,
 } from 'react-native';
 import { List } from 'react-native-paper';
@@ -27,13 +28,13 @@ const DATA = [
   },
 ];
 
-const Item = ({title}) => {
-  console.warn(title)
-  return(
+const Item = ({elemento: item}) => {
+   return(
   <List.Item
-    title={title.titulo}
-    description={title.descricao}
-    left={props => <List.Icon {...props} icon="folder" />}
+    title={item.titulo}
+    description={item.descricao}
+    // left={props => <List.Icon {...props} icon="folder" />}
+    left={props => <Image {...props} style={styles.tinyLogo} source={{uri:'https://www.alura.com.br/artigos/assets/tipos-de-testes-principais-por-que-utiliza-los/tipos-de-testes-principais-por-que-utiliza-los.jpg'}}/>} 
   />
 )};
 
@@ -42,7 +43,7 @@ const App = () => {
     <SafeAreaView style={styles.container}>
       <FlatList
         data={DATA}
-        renderItem={({item}) => <Item title={item} />}
+        renderItem={({item}) => <Item elemento={item} />}
         keyExtractor={item => item.id}
       />
     </SafeAreaView>
@@ -63,6 +64,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 32,
   },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  }
 });
 
 export default App;
